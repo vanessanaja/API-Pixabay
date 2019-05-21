999bf525a309ba18268abea817f3ae66f0a9fe56
Vanessa Naja <vanessacnaja@gmail.com>
2019-05-21 11:56:57 -0700

    add getImages function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 //js/app.js
 import './assets/scss/app.scss';
 
-const apiKey = "11809035-3189d9575e1b430113762ca91";
-let searchTerm = document.getElementById("searchTerm").value;
-const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}&image_type=photo`;
+
+
+const getImages = () => {
+    const searchTerm = document.getElementById("searchTerm").value;
+    const apiKey = "11809035-3189d9575e1b430113762ca91";
+    const URL = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}`;
+    console.log(searchTerm);
+    console.log(URL);
+    fetch(URL)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+        });
+}
+
+document.getElementById("searchButton").addEventListener("click", getImages);
 
  
