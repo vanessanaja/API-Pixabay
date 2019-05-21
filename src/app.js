5e0c9ac377ae22a6b3f07a073e2ae51fb057165c
Vanessa Naja <vanessacnaja@gmail.com>
2019-05-21 11:13:49 -0700

    api request url

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 //js/app.js
 import './assets/scss/app.scss';
 
-let happy = 5;
-
-console.log('Your JS is working' + happy);
+const apiKey = "11809035-3189d9575e1b430113762ca91";
+let searchTerm = document.getElementById("searchTerm").value;
+const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}&image_type=photo`;
 
  
